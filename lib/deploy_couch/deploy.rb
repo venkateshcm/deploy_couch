1a99a08470a37485b59188dec199d45d8058c79d
venky <venkatesh.swdev@gmail.com>
2011-07-22 15:10:49 +0530

    added executable to run deploy_couch

--- lib/deploy_couch/deploy.rb
+++ lib/deploy_couch/deploy.rb
@@ -16,17 +16,27 @@ module DeployCouch
       last_applied_delta_id = applied_deltas[-1] if applied_deltas.count > 0
       delta_keys_to_apply = all_delta_keys.select {|key| key > last_applied_delta_id }
     
-      applied_deltas = []
+      deltas = []
+      puts "Deltas #{all_delta_keys} found"
+      puts "Already applied deltas #{applied_deltas}"
+      puts "Applying deltas #{delta_keys_to_apply}"
+      puts ""
+      puts "Start applying deltas"
     
       delta_keys_to_apply.each do |key|
         delta = deltas_map[key]
         type = couch_schema.get_next_type_version_for(delta.type)
         DeltaProcessor.new(type,@config,delta,repository).apply
         couch_schema.completed(delta)
-        applied_deltas.push(delta)
-      end  
+        deltas.push(delta)
+        puts "applied delta #{delta.file_name}"
+      end
+
+      puts "Finished applying deltas"
+      puts ""
+      puts "Deltas #{all_delta_keys - applied_deltas - delta_keys_to_apply} where skiped"
     
-      applied_deltas
+      deltas
     end
   
   end
